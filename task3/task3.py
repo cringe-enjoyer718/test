21770264e620665ce6dd348c9dfa90f8b2df1e27
Веретенникова Анастасия <115228188+cringe-enjoyer718@users.noreply.github.com>
2024-04-23 15:17:05 +0400

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,9 +1,13 @@
 import json
+import sys
+values_f = sys.argv[1]
+tests_f = sys.argv[2]
+report_f = sys.argv[3]
 
-with open('values.json', 'r') as values_file:
+with open(values_f, 'r') as values_file:
     values_data = json.load(values_file)
 
-with open('tests.json', 'r') as tests_file:
+with open(tests_f, 'r') as tests_file:
     tests_data = json.load(tests_file)
 
 testss = tests_data['tests']
@@ -16,6 +20,6 @@ for test_item in testss:
 
 
 
-with open('report.json', 'w') as report_file:
+with open(report_f, 'w') as report_file:
     json.dump(tests_data, report_file, indent=4)
 print("Результат записан в файл report.json")
